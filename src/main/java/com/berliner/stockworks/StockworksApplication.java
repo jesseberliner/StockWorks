63bdee5f74e2da21e9d1816a4521d69069d1cf9f
JesseBerliner <jesseberliner@gmail.com>
2017-12-31 17:11:16 -0500

    Initial commit-no changes

---
+++ src/main/java/com/berliner/stockworks/StockworksApplication.java
@@ -0,0 +1,12 @@
+package com.berliner.stockworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockworksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockworksApplication.class, args);
+	}
+}
---
+++ src/test/java/com/berliner/stockworks/StockworksApplicationTests.java
@@ -0,0 +1,16 @@
+package com.berliner.stockworks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StockworksApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
