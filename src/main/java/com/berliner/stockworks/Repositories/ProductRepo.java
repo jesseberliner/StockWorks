731a2cdcebc4bff88ffffe248f6fd624712f2f47
Jesse <jesseberliner@gmail.com>
2018-08-01 15:17:20 -0400

    Commit for pull

--- src/main/java/com/berliner/stockworks/Repositories/ProductRepo.java
+++ src/main/java/com/berliner/stockworks/Repositories/ProductRepo.java
@@ -3,6 +3,9 @@ package com.berliner.stockworks.Repositories;
 import com.berliner.stockworks.Models.Product;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Set;
+
 public interface ProductRepo extends CrudRepository<Product, Long>
 {
+    Set<Product> findByDeletedIs(boolean value);
 }
