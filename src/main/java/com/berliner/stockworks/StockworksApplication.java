0665909eb4f78125d77ae88e27123b58f9f0d8fe
Jesse <jesseberliner@gmail.com>
2018-08-03 16:42:40 -0400

    Removed attempt at uploading images

--- src/main/java/com/berliner/stockworks/StockworksApplication.java
+++ src/main/java/com/berliner/stockworks/StockworksApplication.java
@@ -1,30 +1,17 @@
 package com.berliner.stockworks;
 
 
-import com.berliner.stockworks.storage.StorageProperties;
-import com.berliner.stockworks.storage.StorageService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableConfigurationProperties(StorageProperties.class)
 public class StockworksApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StockworksApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner init(StorageService storageService)
-	{
-		return (args) -> {
-			storageService.deleteAll();
-			storageService.init();
-		};
-	}
+
 }
